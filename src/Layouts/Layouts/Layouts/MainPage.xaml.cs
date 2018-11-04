9e1d6259f03e7dd17d381847436a270b92b049b4
Kym Phillpotts <kphillpotts@gmail.com>
2018-11-04 11:26:51 +1100

    Added layout project

---
+++ src/Layouts/Layouts/Layouts/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Layouts
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void overlappingGridButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
